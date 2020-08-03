d7cfee8da98aa7243a015e2387bd25c271b35dd6
HassanKhan123 <hk2602809@gmail.com>
2020-08-03 20:54:53 +0500

    useQueryHook applied

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { useQuery } from "react-query";
+
+import Planet from './Planet'
+
+const fetchPlanets = async () => {
+  const res = await fetch("http://swapi.dev/api/planets");
+  return res.json();
+};
 
 function Planets() {
-    return (
+  const { data, status } = useQuery("planets", fetchPlanets);
+  console.log(data);
+  return (
+    <div>
+      <h2>Planets</h2>
+      {status === "loading" && <div>Loading data...</div>}
+      {status === "success" && (
         <div>
-            <h2>Planets</h2>
+          {data.results.map((planet) => (
+            <Planet planet={planet} key={planet.name}/>
+          ))}
         </div>
-    )
+      )}
+      {status === "error" && <div>Error fetching data</div>}
+    </div>
+  );
 }
 
-export default Planets
+export default Planets;
