8a48e80862bb54627a4e4428738c79ebf3d9be95
HassanKhan123 <hk2602809@gmail.com>
2020-08-04 06:16:16 +0500

    pagination added using usePaginatedQuery

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useQuery } from "react-query";
+import { usePaginatedQuery } from "react-query";
 
 import Planet from "./Planet";
 
@@ -10,24 +10,43 @@ const fetchPlanets = async (key, page) => {
 
 function Planets() {
   const [page, setPage] = useState(1);
-  const { data, status } = useQuery(["planets", page], fetchPlanets, {
-    staleTime: 0,
-    onSuccess: () => console.log("data fetched"),
-  });
-  console.log(data);
+  const { resolvedData, latestData, status } = usePaginatedQuery(
+    ["planets", page],
+    fetchPlanets,
+    {
+      staleTime: 0,
+      onSuccess: () => console.log("data fetched"),
+    }
+  );
   return (
     <div>
       <h2>Planets</h2>
-      <button onClick={() => setPage(1)}>Page 1</button>
-      <button onClick={() => setPage(2)}>Page 2</button>
-      <button onClick={() => setPage(3)}>Page 3</button>
       {status === "loading" && <div>Loading data...</div>}
       {status === "success" && (
-        <div>
-          {data.results.map((planet) => (
-            <Planet planet={planet} key={planet.name} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous Page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() =>
+              setPage((old) =>
+                !latestData || !latestData.next ? old : old + 1
+              )
+            }
+            disabled={!latestData || !latestData.next}
+          >
+            Next Page Page
+          </button>
+          <div>
+            {resolvedData.results.map((planet) => (
+              <Planet planet={planet} key={planet.name} />
+            ))}
+          </div>
+        </>
       )}
       {status === "error" && <div>Error fetching data</div>}
     </div>
